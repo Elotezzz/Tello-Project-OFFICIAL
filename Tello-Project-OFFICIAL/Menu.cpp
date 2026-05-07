c3c2518122e779eb785326e88a5fbc2470928ae5
Kale <khaled.yair.loarte.macedo@gmail.com>
2026-05-06 23:31:44 -0500

    PASANDO INFO

--- Tello-Project-OFFICIAL/Menu.cpp
+++ Tello-Project-OFFICIAL/Menu.cpp
@@ -1,1 +1,37 @@
 #include "Menu.h"
+
+Menu::Menu(const char* titulo, const char* opciones[], int cantidad) : titulo(titulo), opciones(opciones),
+cntOpciones(cantidad), seleccion(0) {
+}
+
+int Menu::mostrarMenu() {
+	char tecla;
+	while (true) {
+		system("cls");
+		std::cout << "====== " << titulo << " ======\n";
+		//recorremos el arreglo de opciones[] y preguntamos si la seleccion es igual a i 
+		//(ejemplos 1. ingresar) se colocara la >; sino se colocara un espacio
+		for (int i = 0; i < cntOpciones; i++) {
+			if (seleccion == i) {
+				std::cout << "> " << i + 1 << ". " << opciones[i] << "\n";
+			}
+			else {
+				std::cout << "  " << i + 1 << ". " << opciones[i] << "\n";
+			}
+		}
+
+		tecla = _getch();
+		if (tecla == -32) {
+			tecla = _getch();
+			if (tecla == KEY_DOWN) {
+				seleccion = (seleccion + 1) % cntOpciones; //lol 30 minutos en esto
+			}
+			else if (tecla == KEY_UP) {
+				seleccion = (seleccion - 1 + cntOpciones) % cntOpciones;
+			}
+		}
+		else if (tecla == KEY_ENTER) {
+			return seleccion;
+		}
+	}
+};
--- Tello-Project-OFFICIAL/Tello-Project-OFFICIAL.cpp
+++ Tello-Project-OFFICIAL/Tello-Project-OFFICIAL.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <cstdlib>
+#include <windows.h>
+#include "Menu.h"
 
 using namespace std;
 
+//void menuInsertar() {
+//	const char* opciones[] = { "Al inicio", "Al final", "En posición" };
+//	int op = mostrarMenu("INSERTAR", opciones, 3);
+//
+//	switch (op) {
+//	case 0: 
+//
+//		break;
+//	case 1: 
+//
+//		break;
+//	case 2: 
+//
+//		break;
+//	}
+//}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int opc;
+
+	//CREAR MENU =======
+	const char* opciones[] = { "Crear Tablero", "Ver Tablero", "Eliminar Tablero", "Seleccionar Tablero", "Salir" };
+	Menu menuPrincipal("TELLO", opciones, 5);
+	//==================
+
+	do {
+		opc = menuPrincipal.mostrarMenu();
+		switch (opc) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	} while (opc != 4);
+	return 0;
 }
